fc34fb4eae02c6c0d1a92335999fae8011480dfa
Arvin Malaluan <malaluanofficial7@gmail.com>
2023-11-05 10:03:44 +0800

    Update asgi.py

--- main/asgi.py
+++ main/asgi.py
@@ -4,11 +4,8 @@ from django.core.asgi import get_asgi_application
 from chat.routing import websocket_urlpatterns
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-import errands.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
-django.setup()
-
 
 django_asgi_application = get_asgi_application()
 
