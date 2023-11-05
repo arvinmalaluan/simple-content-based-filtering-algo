66c8f2cc14e9e3f4e34c152bb15edfd3bd315177
Arvin Malaluan <malaluanofficial7@gmail.com>
2023-11-05 09:24:23 +0800

    nc

--- main/asgi.py
+++ main/asgi.py
@@ -1,11 +1,10 @@
+import errands.routing
+from django.core.asgi import get_asgi_application
 from chat.routing import websocket_urlpatterns
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 import os
 import django
-
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-
-from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 django.setup()
