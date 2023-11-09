06b50412a3032eb44a86a003ff8f18da9641ffa7
Arvin Malaluan <malaluanofficial7@gmail.com>
2023-11-09 09:44:15 +0800

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -148,13 +148,12 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
-
+SECURE_SSL_REDIRECT = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_ROOT = 'media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = BASE_DIR / 'staticfiles'
