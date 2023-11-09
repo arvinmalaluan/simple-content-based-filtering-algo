f71763900e6868ed429a6eb22fe57fd73bd1c217
Arvin Malaluan <malaluanofficial7@gmail.com>
2023-11-09 12:49:35 +0800

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -148,7 +148,6 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
-SECURE_SSL_REDIRECT = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
