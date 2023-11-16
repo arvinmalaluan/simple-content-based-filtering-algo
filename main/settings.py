4eb13504ce3c7fc698271d0632eb1d631bb3cc88
Arvin Malaluan <malaluanofficial7@gmail.com>
2023-11-17 05:44:11 +0800

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -23,7 +23,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'malaluanofficial7@gmail.com'
-EMAIL_HOST_PASSWORD = 'nnrzonadktgtxiwx'
+EMAIL_HOST_PASSWORD = 'nnrz onad ktgt xiwx'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
