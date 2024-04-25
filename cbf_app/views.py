ac7356198f907e4d687dad89d61542c3958813c9
Arvin Malaluan <malaluanofficial7@gmail.com>
2024-04-26 02:47:37 +0800

    change

--- cbf_app/views.py
+++ cbf_app/views.py
@@ -4,15 +4,15 @@ from .ml_model import provide_recommendation, provide_compatibility_apply
 
 @api_view(['POST'])
 def provide_compatible_jobs(request):
-  post = request.data['payload']['skills']
+  post = request.data['skills']
   x = provide_recommendation(post)
   return Response({"positions": x})
 
 
 @api_view(['POST'])
 def provide_compatibility(request):
-  job_title = request.data['payload']['jobtitle']
-  skills = request.data['payload']['skills']
+  job_title = request.data['jobtitle']
+  skills = request.data['skills']
 
   x = provide_compatibility_apply(job_title, skills)
   return Response({"compatibility": x})
